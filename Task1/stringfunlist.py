837e0f9a23a1e632e9b641c28c51882f4231e912
triveniy2 <ytriveni@gmail.com>
2024-06-24 22:25:56 +0530

    functions

---
+++ Task1/stringfunlist.py
@@ -0,0 +1,3 @@
+str1 = "Hello World" [: : -1]
+print(str1)
+
---
+++ Task1/table.py
@@ -0,0 +1,8 @@
+n = int(input("enter number to display table"))
+for i in range(1,11):
+    print( i ,"X ",n, "=", i*n)
+    i += 1
+    if i == 10:
+        continue
+        
+
